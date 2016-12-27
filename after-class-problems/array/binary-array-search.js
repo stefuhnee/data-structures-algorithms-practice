76416b76e2c0da909b32e03a3acf603854b3601f
Stefanie Hansen <stefuhnee@gmail.com>
2016-12-26 18:59:15 -0800

    indentation fixed

--- after-class-problems/array/binary-array-search.js
+++ after-class-problems/array/binary-array-search.js
@@ -1,26 +1,28 @@
+'use strict';
+
 let testArray = [-5, -1, 0, 3, 5, 8, 12, 15];
 
 function binarySearch(arr, n) {
- let mid;
+  let mid;
 
- return _binarySearch(0, arr.length - 1);
+  return _binarySearch(0, arr.length - 1);
 
- function _binarySearch(arrStart, arrEnd) {
-   mid = Math.floor((arrStart + arrEnd) / 2);
+  function _binarySearch(arrStart, arrEnd) {
+    mid = Math.floor((arrStart + arrEnd) / 2);
 
-   console.log(arr[mid]);
-   console.log(n);
+    console.log(arr[mid]);
+    console.log(n);
 
-   if (arr[mid] === n) {
-     return mid;
-   }
+    if (arr[mid] === n) {
+      return mid;
+    }
+    
+    if (arrStart === arrEnd && arr[mid] !== n) return false;
 
-   if (arrStart === arrEnd && arr[mid] !== n) return false;
-
-   if (n > arr[mid]) return _binarySearch(mid + 1, arrEnd);
-   if (n < arr[mid]) return _binarySearch(arrStart, mid - 1);
-   return;
- }
+    if (n > arr[mid]) return _binarySearch(mid + 1, arrEnd);
+    if (n < arr[mid]) return _binarySearch(arrStart, mid - 1);
+    return;
+  }
 }
 
 console.log(binarySearch(testArray, 0));
